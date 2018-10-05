d2bc3ca78a8626e91a0557c860ccbc212a0a4c8b
taosun2 <taosun2@illinois.edu>
2018-10-06 07:08:03 +0800

    lc745 using trie

---
+++ leetcode-notes/lc745-PrefixSuffixSearch.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Trie {
+private:
+    class TrieNode{
+    public:    
+        bool isWord;
+        int index;
+        TrieNode * children[27];
+        
+        TrieNode(){
+            isWord = false;
+            index = -1;
+            for (int i=0; i<27; i++)
+                children[i] = NULL;
+        }
+    };
+    
+    TrieNode * root;
+    
+    
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word, int index) {
+        TrieNode * tmp = root;
+        for (int i=0; i<word.size(); i++){
+            int idx = word[i] - 'a';
+            if (tmp->children[idx] == NULL)
+                tmp->children[idx] = new TrieNode();
+            tmp = tmp->children[idx];
+            tmp->index = index;
+        }
+        tmp->isWord = true;
+    }
+    
+    int startsWith(string all_prefix) {    
+        TrieNode * tmp = root;
+        for (int i=0; i<all_prefix.size(); i++){
+            int idx = all_prefix[i] - 'a';
+            if (tmp->children[idx] == NULL) return -1;
+            tmp = tmp->children[idx];
+        }
+        return tmp->index;
+    }
+};
+
+
+class WordFilter {
+private:
+    Trie trie;
+    
+public:
+    WordFilter(vector<string> words) {
+        for (int i=0; i<words.size(); ++i){
+            string w = words[i];
+            string key = '{' + w;
+            trie.insert(key, i);
+            for (int j=0; j<w.size(); ++j){
+                key = w[w.size()-j-1] + key;
+                trie.insert(key, i);
+            }
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        return trie.startsWith(suffix + "{" + prefix);        
+    }
+};
+
+
+int main(){
+    vector<string> words({"apple", "aple", "Hello", "alle"});
+    WordFilter *obj = new WordFilter(words);
+    cout << obj->f("a", "le") << endl;
+    return 0;
+}
