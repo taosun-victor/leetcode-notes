b727cc094b6c85f0c47104fda6341cb467cf96ba
Tao Sun <taosun.victor@gmail.com>
2020-07-20 12:33:07 -0700

    add new sol to partition subsets

--- leetcode-notes/partition_subsets_equal_sum.cpp
+++ leetcode-notes/partition_subsets_equal_sum.cpp
@@ -37,8 +37,66 @@ bool canPartitionKSubsets(vector<int>& nums, int k) {
     return dfs(nums, 0, k, 0, totSum / k, 0, visit);
 }
 
+
+//================================================================================
+bool checkSum(vector<int>& sumLeft){
+    for (int s : sumLeft){
+        if (s != 0)
+            return false;
+    }
+    return true;
+} 
+
+bool helper(vector<int>& nums, int idx, vector<int>& sumLeft, vector<int>& pos, int k){
+    if (checkSum(sumLeft))
+        return true;
+    
+    if (idx < 0)
+        return false;
+    
+    bool res = false;
+    for (int i = 0; i < k; i++){
+        if (!res && sumLeft[i] - nums[idx] >= 0){
+            pos[idx] = i + 1;
+            sumLeft[i] -= nums[idx];
+            res = helper(nums, idx - 1, sumLeft, pos, k);
+            sumLeft[i] += nums[idx];
+        }
+    }
+    return res;
+}
+
+bool canPartitionKSubsets_Sol2(vector<int>& nums, int k) {
+    int n = nums.size();
+    if (n < k)
+        return false;
+    
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if (sum % k != 0)
+        return false;
+    
+    vector<int> pos(n), sumLeft(k);
+    for (int i = 0; i < k; i++)
+        sumLeft[i] = sum / k;
+    
+    bool res = helper(nums, n - 1, sumLeft, pos, k);
+    if (res){
+        for (int i = 0; i < k; i++){
+            cout << "Pattern " << i << ": ";
+            for (int j = 0; j < n; j++){
+                if (pos[j] == i + 1)
+                    cout << nums[j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return res;
+}
+
+
 int main() {
+	//vector<int> input{4, 3, 2, 3, 5, 2, 1};
 	vector<int> input{-1,1,0,0};
 	int k = 4;
-	cout << canPartitionKSubsets(input, k) << endl;
+	cout << canPartitionKSubsets_Sol2(input, k) << endl;
 }
